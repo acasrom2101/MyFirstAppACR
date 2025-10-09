4aba31e196ef278ab0ed47d013d2c2bf1100d95c
Riggs <acasrom2101@iescarrillo.es>
2025-10-09 17:50:02 +0200

    Finalización activity 2, inicio activity 3

--- app/src/main/java/es/iescarrillo/myfirstappacr/SecondActivity.java
+++ app/src/main/java/es/iescarrillo/myfirstappacr/SecondActivity.java
@@ -1,6 +1,11 @@
 package es.iescarrillo.myfirstappacr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSegundoEnviarATercero;
+    EditText campoTexto;
+    EditText campoEntero;
+    EditText campoDecimal;
+    Switch interruptorBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,23 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnSegundoEnviarATercero = findViewById(R.id.btnSecondSendFormToThird);
+        campoTexto = findViewById(R.id.campoTexto);
+        campoEntero = findViewById(R.id.campoEntero);
+        campoDecimal = findViewById(R.id.campoDecimal);
+        interruptorBoolean = findViewById(R.id.interruptorBoolean);
+
+        btnSegundoEnviarATercero.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent segundaActivity = new Intent(v.getContext(), ThirdActivity.class);
+        segundaActivity.putExtra("datosCampoTexto", campoTexto.getText().toString());
+        segundaActivity.putExtra("datosCampoEntero", campoEntero.getText().toString());
+        segundaActivity.putExtra("datosCampoDecimal", campoDecimal.getText().toString());
+        segundaActivity.putExtra("posicionInterruptor", interruptorBoolean.isChecked());
+        startActivity(segundaActivity);
     }
 }
